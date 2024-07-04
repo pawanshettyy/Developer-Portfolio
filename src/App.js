a4b012b8e77e5b0e9df24624b3dc6196b249444c
Binil B <binil.b.re@gmail.com>
2024-07-04 13:12:12 +0530

    mouse follower

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import Body from './Component/Body';
-import { DataContextProvider } from './Component/Data/Context';
-import { MouseProvider } from './Component/Data/MouseContext';
+import "./App.css";
+import Body from "./Component/Body";
+import { DataContextProvider } from "./Component/Data/Context";
+
+import CursorFollow from "./Component/mouseFollower";
 
 function App() {
   return (
     <div className="">
       <DataContextProvider>
-        <MouseProvider>
-          <Body/>
-        </MouseProvider>
+      <CursorFollow />
+        <Body />
       </DataContextProvider>
     </div>
   );
---
+++ src/Component/mouseFollower.js
@@ -0,0 +1,11 @@
+import Cursor from "./cursor";
+
+const CursorFollow = () => {
+  return (
+    <div className="app">
+      <Cursor/>
+    </div>
+  );
+};
+
+export default CursorFollow;
